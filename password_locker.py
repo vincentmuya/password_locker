a4974ac635dd4a337050c25e6df27ad995bb151a
vincentmuya <vincentmuya13@gmail.com>
2018-01-18 21:08:41 +0300

    created a password class and defined the property object

---
+++ password_locker.py
@@ -0,0 +1,16 @@
+class Password:
+    '''
+    class that generates new instances of Passwords
+    '''
+
+def __init__(self,account_name,user_name,email):
+    '''
+    __init__method that helps us define properties for our object
+
+    Args:
+        account_name: New password account_name.
+        user_name: New password user_name.
+        email:New password email
+
+
+    '''
